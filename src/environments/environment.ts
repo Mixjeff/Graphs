5e8b650a3440fa49c1b7ed6cf22e451005447d41
Jeff.Portillo <mixjeff@hotmail.com>
2023-11-10 08:56:14 -0600

    Repo completo

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+//Api donde se extrae la info
+export const environment = {
+  production: false,
+  ceo: 'https://pkgstore.datahub.io/core/co2-fossil-by-nation/fossil-fuel-co2-emissions-by-nation_json/data/2b4874bb29c461a614e92773956ad573/fossil-fuel-co2-emissions-by-nation_json.json',
+
+};
+
